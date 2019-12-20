87a78b96fd24dff865c012b0de23755f14e26fb6
x086893 <harshad.bankar@klm.com>
2019-12-20 16:37:10 +0100

    serve angular from html

--- server.js
+++ server.js
@@ -36,5 +36,9 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
+app.get('*', (req, res) => {
+    res.type('.html');
+    res.sendFile('./apollo-experiment/dist/apollo-experiment/index.html');
+});
 app.listen(process.env.PORT || 4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
